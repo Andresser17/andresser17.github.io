e3abb1768143729e0e87cdbf685cb24838eecfc7
Andresser17 <andresserserrano2020@gmail.com>
2022-08-25 11:46:53 -0400

    style: change css variables colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bg: "var(--bg)",
-        text: "var(--text)",
-        hover: "var(--hover)",
-        active: "var(--active)",
-        focus: "var(--focus)",
-        "focus-border": "var(--focus-border)",
-        border: "var(--border)",
-        disabled: "var(--disabled)",
-        "disabled-text": "var(--disabled-text)",
-        "disabled-border": "var(--disabled-border)",
+        bg: "rgb(var(--bg) / <alpha-value>)",
+        text: "rgb(var(--text) / <alpha-value>)",
+        hover: "rgb(var(--hover) / <alpha-value>)",
+        active: "rbg(var(--active) / <alpha-value>)",
+        focus: "rgb(var(--focus) / <alpha-value>)",
+        outline: "rgb(var(--outline) / <alpha-value>)",
+        border: "rgb(var(--border) / <alpha-value>)",
       },
     },
   },
   plugins: [],
   corePlugins: {
     preflight: true,
-  }
+  },
 };
