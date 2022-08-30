2dd581c7e1fc2e2566780c0be32a8b8eb4456168
Andresser17 <andresserserrano2020@gmail.com>
2022-08-30 15:33:31 -0400

    refactor: update About information

--- src/routes/About.js
+++ src/routes/About.js
@@ -12,35 +12,38 @@ function About() {
             className="object-cover rounded w-60 h-64 mb-4 m-auto sm:float-left sm:mr-8"
             alt="Author"
           />
-          <span className="block mb-4 text-lg font-semibold">
-            Hi, I'm Alejandro
+          <span className="block mb-2 text-xl font-semibold">
+            Hello, my name is Alejandro Serrano.
           </span>
-          <p className="leading-8">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+          <p className="leading-8 text-xl">
+            Young Software engineer with more than Three years of experience
+            learning programming as a hobby, with experience working with a team
+            in academic projects applying SCRUM methodologies and Gitflow. With
+            passion for clean code practices, and collaborate to Open Source.
           </p>
         </div>
         <div className="mb-8 w-full max-w-[40rem]">
           <h3 className="mb-4 text-3xl font-semibold sm:text-4xl">Skills</h3>
-          <p className="leading-8">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
-          </p>
+          <ul className="[&>li]:leading-8 text-xl">
+            <li>
+              Proficiency with the React and associated libraries (React-Router,
+              Redux).
+            </li>
+            <li>More than Three years of experience with HTML/CSS and JS.</li>
+            <li>Design and Creation of REST APIs using ExpressJS.</li>
+            <li>Experience with NodeJS.</li>
+            <li>Experience with Python.</li>
+            <li>Experience with WordPress and PHP.</li>
+          </ul>
         </div>
         <div className="mb-8 w-full max-w-[40rem]">
           <h3 className="mb-4 text-3xl font-semibold sm:text-4xl">Work</h3>
-          <p className="leading-8">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+          <p className="leading-8 text-xl">
+            Experience working on academic projects applying the best practices,
+            and learning new technologies for solving the daily problems of a
+            Software Developer. I have experience working on an E-commerce
+            project collaborating with more than six co-workers applying SCRUM
+            methodologies and a correct Gitflow using GitHub.
           </p>
         </div>
       </div>
